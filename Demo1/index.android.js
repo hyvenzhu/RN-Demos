cfa9f75a8d928ff752f3862f6907e6d9b6184389
hiphonezhu <hiphonezhu@gmail.com>
2016-05-13 15:57:59 +0800

    first commit

---
+++ Demo1/index.android.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var React = require('react-native');
+var {
+  Text,
+  View
+} = React;
+
+class ReactModule extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.hello}>Hello, World</Text>
+      </View>
+    )
+  }
+}
+var styles = React.StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  hello: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
+
+React.AppRegistry.registerComponent('ReactModule', () => ReactModule);
