02b9bc49863c49c0be1f56090debf0cabe240fca
hiphonezhu <hiphonezhu@gmail.com>
2016-06-15 17:59:00 +0800

    flex test

--- Demo5/index.android.js
+++ Demo5/index.android.js
@@ -4,26 +4,25 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component} from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
 } from 'react-native';
-
 class Demo5 extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+        <Text style={styles.r}>
+          R
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
+        <Text style={styles.g}>
+          G
         </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
+        <Text style={styles.b}>
+          B
         </Text>
       </View>
     );
@@ -33,19 +32,32 @@ class Demo5 extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flexDirection: 'row', // column：垂直排列（垂直是主轴，水平是交叉轴）  row：横向排列（水平是主轴，垂直是交叉轴）
+    justifyContent: 'center', // 设置子布局在主轴方向位置
+    alignItems: 'center', // 设置子布局在交叉轴方向位置
+    backgroundColor: '#000000',
+    flexWrap: 'wrap', // 水平或垂直布局时：如果子View放不下，则自动换行, 默认为'nowrap'
+    borderBottomWidth: 10, // 边界线条粗细
+    borderColor: '#FF0000', // 边界线条颜色
   },
-  welcome: {
+  r: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    backgroundColor: '#FF0000',
+    color: '#FFFFFF',
+    alignSelf: 'flex-start', // 当前View在交叉轴方向位置
   },
-  instructions: {
+  g: {
+    fontSize: 20,
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    backgroundColor: '#00FF00',
+    color: '#FFFFFF',
+  },
+  b: {
+    fontSize: 20,
+    textAlign: 'center',
+    backgroundColor: '#0000FF',
+    color: '#FFFFFF',
   },
 });
 
