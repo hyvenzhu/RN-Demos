4794e0b9ade080e4cd9e0e28a7ab6cd0c47d9a0e
hiphonezhu <hiphonezhu@gmail.com>
2016-06-20 22:10:24 +0800

    update Demo5

--- Demo5/index.android.js
+++ Demo5/index.android.js
@@ -32,20 +32,15 @@ class Demo5 extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row', // column：垂直排列（垂直是主轴，水平是交叉轴）  row：横向排列（水平是主轴，垂直是交叉轴）
-    justifyContent: 'center', // 设置子布局在主轴方向位置
-    alignItems: 'center', // 设置子布局在交叉轴方向位置
+    flexDirection: 'column', // column：垂直排列（垂直是主轴，水平是交叉轴）  row：横向排列（水平是主轴，垂直是交叉轴）
     backgroundColor: '#000000',
     flexWrap: 'wrap', // 水平或垂直布局时：如果子View放不下，则自动换行, 默认为'nowrap'
-    borderBottomWidth: 10, // 边界线条粗细
-    borderColor: '#FF0000', // 边界线条颜色
   },
   r: {
     fontSize: 20,
     textAlign: 'center',
     backgroundColor: '#FF0000',
     color: '#FFFFFF',
-    alignSelf: 'flex-start', // 当前View在交叉轴方向位置
   },
   g: {
     fontSize: 20,
@@ -58,6 +53,9 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     backgroundColor: '#0000FF',
     color: '#FFFFFF',
+    position: 'relative',
+    left: 100,
+    top: 100
   },
 });
 
