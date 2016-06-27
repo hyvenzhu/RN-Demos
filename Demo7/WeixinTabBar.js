4508723647d823dee9c444e59a08900e46d3330e
hiphonezhu <hiphonezhu@gmail.com>
2016-06-27 17:51:23 +0800

    code format

--- Demo7/WeixinTabBar.js
+++ Demo7/WeixinTabBar.js
@@ -1,12 +1,12 @@
 'use strict';
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import {
-    StyleSheet,
-    View,
-    TouchableOpacity,
-    Text
+	StyleSheet,
+	View,
+	TouchableOpacity,
+	Text
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons/Ionicons';
@@ -14,51 +14,55 @@ import Icon from 'react-native-vector-icons/Ionicons';
 class WeixinTabBar extends Component {
 
 	propTypes = {
-    goToPage: React.PropTypes.func,
-    activeTab: React.PropTypes.number,
-    tabs: React.PropTypes.array,
+		goToPage: React.PropTypes.func,
+		activeTab: React.PropTypes.number,
+		tabs: React.PropTypes.array,
 	}
 
-  setAnimationValue({value})
-  {
-    
-  }
+	setAnimationValue({value}) {
 
-  renderTabOption(tab, i)
-  {
-    return (
-        <TouchableOpacity onPress={()=>this.props.goToPage(i)} key={tab} style={styles.tab}>
-            <Icon 
-                name={tab}
-                size={30}/>
-            <Text>
-                {tab.tabName}
-            </Text>
-        </TouchableOpacity>
-    );
-  }
+	}
+
+	renderTabOption(tab, i) {
+		return (
+			<TouchableOpacity onPress={()=>this.props.goToPage(i)} key={tab} style={styles.tab}>
+				<View style={styles.tabItem}>
+					<Icon
+						name={tab}
+						size={30}/>
+					<Text>
+						{tab.tabName}
+					</Text>
+				</View>
+			</TouchableOpacity>
+		);
+	}
 
 	render() {
-    return (
-      <View style={styles.tabs}>
-          {this.props.tabs.map((tab, i) => this.renderTabOption(tab, i))}
-      </View>
-    );
-  }
+		return (
+			<View style={styles.tabs}>
+				{this.props.tabs.map((tab, i) => this.renderTabOption(tab, i))}
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-    tabs: {
-      flexDirection: 'row',
-      height: 50,
-    },
+	tabs: {
+		flexDirection: 'row',
+		height: 50,
+	},
 
-    tab: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      flexDirection: 'column'
-    }
+	tab: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+
+	tabItem: {
+		flexDirection: 'column',
+		alignItems: 'center',
+	},
 });
 
 
--- Demo7/index.android.js
+++ Demo7/index.android.js
@@ -4,46 +4,46 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+	AppRegistry,
+	StyleSheet,
+	Text,
+	View
 } from 'react-native';
 
 import ScrollableTabView from 'react-native-scrollable-tab-view';
 import WeixinTabBar from './WeixinTabBar';
 
 class Demo7 extends Component {
-  render() {
-    return (
-      <ScrollableTabView
-        renderTabBar={() => <WeixinTabBar/>}
-        tabBarPosition ='bottom'>
-          <Text tabLabel ='ios-paper' style={styles.content}>
-              #1
-          </Text>
-          <Text tabLabel ='ios-people' style={styles.content}>
-              #2
-          </Text>
-          <Text tabLabel ='ios-paper-plane' style={styles.content}>
-              #3
-          </Text>
-          <Text tabLabel ='ios-person-add' style={styles.content}>
-              #4
-          </Text>
-      </ScrollableTabView>
-    );
-  }
+	render() {
+		return (
+			<ScrollableTabView
+				renderTabBar={() => <WeixinTabBar/>}
+				tabBarPosition='bottom'>
+				<Text tabLabel='ios-paper' style={styles.content}>
+					#1
+				</Text>
+				<Text tabLabel='ios-people' style={styles.content}>
+					#2
+				</Text>
+				<Text tabLabel='ios-paper-plane' style={styles.content}>
+					#3
+				</Text>
+				<Text tabLabel='ios-person-add' style={styles.content}>
+					#4
+				</Text>
+			</ScrollableTabView>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-    content: {
-        alignSelf: 'center',
-        flex: 1,
-        backgroundColor: '#FF0000'
-    }
+	content: {
+		alignSelf: 'center',
+		flex: 1,
+		backgroundColor: '#FF0000'
+	}
 });
 
 AppRegistry.registerComponent('Demo7', () => Demo7);
