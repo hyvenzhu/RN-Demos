8d061b2a5000837ec5f541aa31c92ee326c5f7ac
hiphonezhu <hiphonezhu@gmail.com>
2016-07-01 17:49:26 +0800

    init Demo9

--- Demo9/index.android.js
+++ Demo9/index.android.js
@@ -4,49 +4,71 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, {Component} from "react";
+import {AppRegistry, StyleSheet, Text, View, LayoutAnimation, TouchableOpacity, Image} from "react-native";
 
 class Demo9 extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+	// 构造
+	constructor(props) {
+		super(props);
+		// 初始状态
+		this.state = {
+			width: 100,
+			height: 100
+		};
+	}
+
+	onPress() {
+		// LayoutAnimation.configureNext(LayoutAnimation.Presets.linear);
+		LayoutAnimation.configureNext({
+			duration: 7000,   //持续时间
+			update: {
+				type: 'linear',
+				property: 'opacity'
+			},
+		});
+		this.setState({width: this.state.width + 10, height: this.state.height + 10});
+	}
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<Image source={require('./icon.jpg')} style={{width: this.state.width, height: this.state.height}}/>
+
+				<TouchableOpacity style={styles.instructions} onPress={()=>this.onPress()}>
+					<Text style={{alignSelf: 'center', color: '#FFFFFF'}}>
+						Press me!
+					</Text>
+				</TouchableOpacity>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF',
+		flexDirection: 'column'
+	},
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10,
+		backgroundColor: '#00FF00',
+		width: 150,
+		height: 80,
+	},
+	instructions: {
+		backgroundColor: '#FF0000',
+		width: 80,
+		height: 50,
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginTop: 20
+	},
 });
 
 AppRegistry.registerComponent('Demo9', () => Demo9);
